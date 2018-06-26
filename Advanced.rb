b45378e893be652ab1af57ca78bd0f201764aa1b
JRenz12 <jrenzulli@spartglobal.com>
2018-06-27 00:13:20 +0100

    Advanced Square Root

---
+++ Advanced.rb
@@ -0,0 +1,18 @@
+class Advanced_Calculator
+  attr_accessor :num1
+  # :addition, :subtract, :divide, :multiply
+
+  def initialize num1
+    self.num1 = num1
+    # self.num2 = num2
+  end
+  def square_root
+
+    Math.sqrt(self.num1)
+
+  end
+
+  def power
+    
+  end
+end
--- app.rb
+++ app.rb
@@ -1,18 +1,41 @@
 require_relative 'Basic.rb'
 require_relative 'Advanced.rb'
 
-puts "Insert your first nummber: "
-num1 = gets.to_i
-puts "Insert your second number: "
-num2 = gets.to_i
-puts "Choose your operator: (A)dd, (S)ubtract, (M)ultiply, (D)ivide "
-ops = gets.chomp
+def basic_calculator
+  puts "Insert your first nummber: "
+  num1 = gets.to_i
+  puts "Insert your second number: "
+  num2 = gets.to_i
+  puts "Choose your operator: (A)dd, (S)ubtract, (M)ultiply, (D)ivide "
+  ops = gets.chomp
 
-user1 = Basic_calculator.new num1, num2, ops
-user2 = Advanced_Calculator.new num1, num2
-puts user1.basic_calculator
-puts user2.advanced_calculator
+  user1 = Basic_calculator.new num1, num2, ops
+  puts user1.basic_calculator
+end
 
+def advanced_calc
+  puts "What number would you like to get the square root of? "
+  num = gets.to_i
+  user2 = Advanced_Calculator.new num
+  puts user2.square_root
+end
+
+
+
+
+
+
+
+puts "What type of calculator would you like to use? (b)asic, 2 for (a)dvanced "
+calc_choice = gets.chomp
+
+if calc_choice == "b" || calc_choice == "B"
+basic_calculator()
+elsif calc_choice == "a" || calc_choice == "A"
+  advanced_calc()
+else
+  puts "Not an option"
+end
 
 
 # puts "What type of calculator would you like to use? (b)asic, 2 for (a)dvanced "
